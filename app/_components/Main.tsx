e7d85ae4daf8f3433549d6eaf86170f3fea3469a
Maxime <maxime.esteves81200@gmail.com>
2025-02-03 15:16:44 +0100

    Update Main.tsx
    Signed-off-by: Maxime <maxime.esteves81200@gmail.com>

--- app/_components/Main.tsx
+++ app/_components/Main.tsx
@@ -2,27 +2,29 @@ import { Experiences } from "./Experiences";
 import { Projets } from "./Projets";
 import { Formation } from "./Formation";
 import { Contact } from "./Contact";
-import { Section } from "./Section"
+import { Skills } from "./Skills"
+import { Footer } from "./Footer";
 
 export const Main = () => {
     
     return (
 
-        <><div>
+        <><div >
             <Projets />
         </div>
-        <div className="grid grid-cols-2 items-start mt-4 gap-4 w-auto">
+        <div className="grid grid-cols-2 items-start mt-4 gap-4 w-auto max-md:grid-cols-1">
             <Formation />
             <Experiences />
         </div>
-        <div className=" flex justify-center">
-          <Section />
+        <div className=" flex justify-center ">
+          <Skills />
       
         </div >
-        <div className=" flex justify-center">
+        <div className=" flex justify-center ">
 
            <Contact /> 
         </div>
+           <Footer />
         
         </>
     );
